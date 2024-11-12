c7707f7fde8885f238e066a89b4b67843d389aa6
Irfan <irfan@keyvalue.systems>
2024-11-12 14:34:20 +0530

    Cleanup

--- lib/main.dart
+++ lib/main.dart
@@ -5,14 +5,6 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 
-/// This sample app shows an app with two screens.
-///
-/// The first route '/' is mapped to [HomeScreen], and the second route
-/// '/details' is mapped to [DetailsScreen].
-///
-/// The buttons use context.go() to navigate to each destination. On mobile
-/// devices, each destination is deep-linkable and on the web, can be navigated
-/// to using the address bar.
 void main() => runApp(const MyApp());
 
 /// The main app.
@@ -78,7 +70,6 @@ class ScaffoldWithNavBar extends StatelessWidget {
   });
 
   /// The widget to display in the body of the Scaffold.
-  /// In this sample, it is a Navigator.
   final Widget child;
 
   @override
